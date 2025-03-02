e9f92efd72efae11d6960b4df1f9db9df84d87c1
jhhocs <joshuaho1@cpp.edu>
2025-03-02 11:47:18 -0800

    Q2: Ensure features are mapped to the same numbers across all training instances

--- Q2/decision_tree_2.py
+++ Q2/decision_tree_2.py
@@ -3,7 +3,7 @@
 # FILENAME: decision_tree_2.py
 # SPECIFICATION: Train, test, and output the performance of the 3 models created by using each training set on the test set provided
 # FOR: CS 4210- Assignment #2
-# TIME SPENT: 1 hour
+# TIME SPENT: 2 hour
 #-----------------------------------------------------------*/
 
 #IMPORTANT NOTE: DO NOT USE ANY ADVANCED PYTHON LIBRARY TO COMPLETE THIS CODE SUCH AS numpy OR pandas. You have to work here only with standard
@@ -14,6 +14,9 @@ from sklearn import tree
 import csv
 dataSets = ['contact_lens_training_1.csv', 'contact_lens_training_2.csv', 'contact_lens_training_3.csv']
 
+#Variables used to ensure features are maped to the same numbers across all training instances 
+first1 = True
+first2 = True
 for ds in dataSets:
 
     dbTraining = []
@@ -33,26 +36,26 @@ for ds in dataSets:
     #For instance Young = 1, Prepresbyopic = 2, Presbyopic = 3, X = [[1, 1, 1, 1], [2, 2, 2, 2], ...]]
     #--> add your Python code here
     num_features = len(row) - 1
-    d = [dict() for x in range(num_features)]
+    if first1:
+        d = [dict() for x in range(num_features)]
     for i, row in enumerate(dbTraining):
-        X.append([])
+        temp = []
         for x in range(num_features):
-            if row[x] in d[x]:
-                X[i].append(d[x].get(row[x]))
-            else:
-                X[i].append(len(d[x]))
+            if first1 and row[x] not in d[x]:
                 d[x][row[x]] = len(d[x])
-
+            temp.append(d[x].get(row[x]))
+        X.append(temp)
+    first1 = False
     #Transform the original categorical training classes to numbers and add to the vector Y.
     #For instance Yes = 1 and No = 2, Y = [1, 1, 2, 2, ...]
     #--> add your Python code here
-    d2 = {}
+    if first2:
+        d2 = {}
     for row in dbTraining:
-        if row[-1] in d2:
-            Y.append(d2.get(row[-1]))
-        else:
-            Y.append(len(d2))
+        if first2 and row[-1] not in d2:
             d2[row[-1]] = len(d2)
+        Y.append(d2.get(row[-1]))
+    first2 = False
 
     #Loop your training and test tasks 10 times here
     average_accuracy = 0
@@ -69,7 +72,7 @@ for ds in dataSets:
             reader = csv.reader(csvfile)
             for i, row in enumerate(reader):
                 if i > 0: #skipping the header
-                    dbTestTraining.append (row)
+                    dbTestTraining.append(row)
                     # print(row)
                 else:
                     features = row
@@ -80,12 +83,13 @@ for ds in dataSets:
         correct = 0
         total = 0
         for i, row in enumerate(dbTestTraining):
-            dbTest.append([])
+            temp = []
             for x in range(num_features):
                 if row[x] in d[x]:
-                    dbTest[i].append(d[x].get(row[x]))
-            dbTest[i].append(d2.get(row[x + 1]))
-        # print(dbTest)
+                    temp.append(d[x].get(row[x]))
+            temp.append(d2.get(row[x + 1]))
+            dbTest.append(temp)
+
         for data in dbTest:
             #Transform the features of the test instances to numbers following the same strategy done during training,
             #and then use the decision tree to make the class prediction. For instance: class_predicted = clf.predict([[3, 1, 2, 1]])[0]
@@ -107,6 +111,3 @@ for ds in dataSets:
     #Your output should be something like that: final accuracy when training on contact_lens_training_1.csv: 0.2
     #--> add your Python code here
     print(f"final accuracy when training on {ds}: {average_accuracy}")
-
-
-
